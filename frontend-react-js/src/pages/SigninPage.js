91b46664bf677e56230b57b6404a68881969b2f1
krishnamohan16 <krishna161299+git@gmail.com>
2023-03-20 10:34:56 +0000

    x ray

--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,32 +1,56 @@
-import './SigninPage.css';
+import './SignupPage.css';
 import React from "react";
 import {ReactComponent as Logo} from '../components/svg/logo.svg';
 import { Link } from "react-router-dom";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
 
-export default function SigninPage() {
+export default function SignupPage() {
 
+  // Username is Eamil
+  const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
+  const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
   const [errors, setErrors] = React.useState('');
 
   const onsubmit = async (event) => {
     event.preventDefault();
     setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
+    console.log('username',username)
+    console.log('email',email)
+    console.log('name',name)
+    try {
+      const { user } = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          name: name,
+          email: email,
+          preferred_username: username,
+        },
+        autoSignIn: { // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        }
+      });
+      console.log(user);
+      window.location.href = `/confirm?email=${email}`
+    } catch (error) {
+        console.log(error);
+        setErrors(error.message)
     }
     return false
   }
 
+  const name_onchange = (event) => {
+    setName(event.target.value);
+  }
   const email_onchange = (event) => {
     setEmail(event.target.value);
+  }
+  const username_onchange = (event) => {
+    setUsername(event.target.value);
   }
   const password_onchange = (event) => {
     setPassword(event.target.value);
@@ -38,18 +62,27 @@ export default function SigninPage() {
   }
 
   return (
-    <article className="signin-article">
-      <div className='signin-info'>
+    <article className='signup-article'>
+      <div className='signup-info'>
         <Logo className='logo' />
       </div>
-      <div className='signin-wrapper'>
+      <div className='signup-wrapper'>
         <form 
-          className='signin_form'
+          className='signup_form'
           onSubmit={onsubmit}
         >
-          <h2>Sign into your Cruddur account</h2>
+          <h2>Sign up to create a Cruddur account</h2>
           <div className='fields'>
-            <div className='field text_field username'>
+            <div className='field text_field name'>
+              <label>Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={name_onchange} 
+              />
+            </div>
+
+            <div className='field text_field email'>
               <label>Email</label>
               <input
                 type="text"
@@ -57,6 +90,16 @@ export default function SigninPage() {
                 onChange={email_onchange} 
               />
             </div>
+
+            <div className='field text_field username'>
+              <label>Username</label>
+              <input
+                type="text"
+                value={username}
+                onChange={username_onchange} 
+              />
+            </div>
+
             <div className='field text_field password'>
               <label>Password</label>
               <input
@@ -68,19 +111,16 @@ export default function SigninPage() {
           </div>
           {el_errors}
           <div className='submit'>
-            <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-            <button type='submit'>Sign In</button>
+            <button type='submit'>Sign Up</button>
           </div>
-
         </form>
-        <div className="dont-have-an-account">
+        <div className="already-have-an-account">
           <span>
-            Don't have an account?
+            Already have an account?
           </span>
-          <Link to="/signup">Sign up!</Link>
+          <Link to="/signin">Sign in!</Link>
         </div>
       </div>
-
     </article>
   );
 }
